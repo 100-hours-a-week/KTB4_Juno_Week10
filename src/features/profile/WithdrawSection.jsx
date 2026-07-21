63c0dddb1403d9bfcfccb17c7f0eb030c7872449
joohee kim <dev.joohee0916@gmail.com>
2026-07-21 16:38:22 +0900

    feat: 탈퇴하기 기능 연결

---
+++ src/features/profile/WithdrawSection.jsx
@@ -0,0 +1,13 @@
+const WithdrawSection = ({ onWithdraw }) => {
+  return (
+    <button
+      type="button"
+      className="mt-12 flex h-10 w-fit cursor-pointer items-center justify-start text-left font-normal text-[#a9a9a9] underline"
+      onClick={onWithdraw}
+    >
+      <span style={{ fontSize: "11px", fontWeight: 400 }}>탈퇴하기</span>
+    </button>
+  );
+};
+
+export default WithdrawSection;
