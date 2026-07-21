69d0a1b830ba551e662e251329196066cf1fb3e6
joohee kim <dev.joohee0916@gmail.com>
2026-07-22 00:15:31 +0900

    refactor: 댓글 API 중복 정의 제거

--- src/api/posts.js
+++ src/api/posts.js
@@ -24,26 +24,6 @@ export const deletePost = (postId) => {
   });
 };
 
-export const createComment = (postId, content) => {
-  return request(`/posts/${postId}/comments`, {
-    method: "POST",
-    body: JSON.stringify({ content }),
-  });
-};
-
-export const updateComment = (postId, commentId, content) => {
-  return request(`/posts/${postId}/comments/${commentId}`, {
-    method: "PATCH",
-    body: JSON.stringify({ content }),
-  });
-};
-
-export const deleteComment = (postId, commentId) => {
-  return request(`/posts/${postId}/comments/${commentId}`, {
-    method: "DELETE",
-  });
-};
-
 export const addLike = (postId) => {
   return request(`/posts/${postId}/likes`, {
     method: "POST",
